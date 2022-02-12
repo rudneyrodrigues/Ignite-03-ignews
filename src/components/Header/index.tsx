9998ec33164096c0ab9f48251cd8b1037d59c3b4
Rudney R <rudney.un2016@icloud.com>
2022-02-12 15:10:31 -0300

    Backup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,11 @@ import { HeaderContainer } from './styles';
 export function Header() {
   return (
     <HeaderContainer>
-      <div className='headerContent'>
+      <div className="headerContent">
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a>Home</a>
+          <a className="active">Home</a>
           <a>Post</a>
         </nav>
       </div>
