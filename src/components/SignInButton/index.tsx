51d17e0086633f15fd529f3834adeec4be822e61
Rudney R <rudney.un2016@icloud.com>
2022-02-22 10:16:43 -0300

    Authentication with Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,30 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
     >
       <FaGithub color='#04d361' />
-      Rudney Rodrigues
-      <FiX color='#737380' className={styles.closeIcon} />
+      {session.user?.name}
+      <FiX
+        color='#737380'
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#eba417' />
       Sign in with Github
