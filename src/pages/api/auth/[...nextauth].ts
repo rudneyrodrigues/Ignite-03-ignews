ebf54a6cb19d8c11be8253e534f2e9c0bded385b
Rudney Rodrigues <rudney.un2016@icloud.com>
2022-02-24 10:38:34 -0300

    Saving users in the database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -16,4 +20,41 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(String(user.email))
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(String(user.email))
+              )
+            )
+          )
+        )
+
+        return true
+      } catch (error) {
+        console.log(error)
+
+        return false
+      }
+
+    },
+  }
 })
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,6 @@
 import { Client } from 'faunadb';
 
 export const fauna = new Client({
-  secret: String(process.env.FAUNADB_KEY)
+  secret: String(process.env.FAUNADB_KEY),
+  // domain: 'db.us.fauna.com', // Esta opção é utilizada para especificar o domínio do servidor FaunaDB
 });
